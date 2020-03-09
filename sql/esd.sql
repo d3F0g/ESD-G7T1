a2eb37dc44530f246b27cad2c32074d72f085275
Josiah <josiahwong.2018@smu.edu.sg>
2020-03-09 11:16:38 +0800

    intial file directory

---
+++ sql/esd.sql
@@ -0,0 +1,90 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `esd`
+--
+DROP DATABASE IF EXISTS `esd`;
+CREATE DATABASE IF NOT EXISTS `esd`;
+USE `esd`;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `social_media` varchar(50),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cafe`
+
+DROP TABLE IF EXISTS `cafe`;
+CREATE TABLE IF NOT EXISTS `cafe` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `avg_review` double(3,2) NOT NULL,
+  `price` double(6,2) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+
+DROP TABLE IF EXISTS `booking`;
+CREATE TABLE IF NOT EXISTS `booking` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `userID` int(10) NOT NULL,
+  `cafeID` int(10) NOT NULL,
+  `dateTime` timestamp NOT NULL,
+  `status` enum('Confirmed', 'Rejected') NOT NULL,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY(`userID`) references users(ID),
+  FOREIGN KEY(`cafeID`) references cafe(ID)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+DROP TABLE IF EXISTS `reviews`;
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `userID` int(10) NOT NULL,
+  `cafeID` int(10) NOT NULL,
+  `content` text NOT NULL,
+  `stars` double(3,2) NOT NULL,
+  PRIMARY KEY (`ID`),
+  FOREIGN KEY(`userID`) references `users`(`ID`),
+  FOREIGN KEY(`cafeID`) references `cafe`(`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
