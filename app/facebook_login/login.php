1d91b9ef4794ab2862af4a66b5b2cd6cb2338229
alvintsz <alvin.tan.2018@sis.smu.edu.sg>
2020-03-23 01:57:44 +0800

    login logic for users to sign up

--- app/facebook_login/login.php
+++ app/facebook_login/login.php
@@ -16,23 +16,26 @@ $fullURL = $handler->getLoginUrl($redirectTo, $data);
 <html>
   <link rel="stylesheet" type="text/css" href="../../css/style.css">
 
-        <form id="msform">
+        <form id="msform" method="post" action="login_logic.php">
         <fieldset>
             <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            <input type="email" class="form-control" name="loginEmail" aria-describedby="emailHelp" placeholder="Enter email">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             <br><br>
             <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            <input type="password" class="form-control" name="loginPassword" placeholder="Password">
           
           
           <input type="submit" value="Login" class="btn btn-primary">
           <input type="button" onclick="window.location = '<?php echo $fullURL ?>'" value="Login with Facebook" class="btn btn-primary">
-          </fieldset>
+          <?php if (isset($_GET["error"])){
+                echo $_GET['error'];
+                } ?>  
+        </fieldset>
         </form>
       
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
         <script src="login_animation.js"></script>
-
 </html>
+
---
+++ app/facebook_login/login_logic.php
@@ -0,0 +1,46 @@
+<?php
+$email = $_POST['loginEmail'];
+$pass = $_POST['loginPassword'];
+
+//Connect to db 
+//Add registration details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "esd";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT * FROM users WHERE email = '$email';";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $db_password = $row['password'];
+    $first_name = $row['first_name'];
+
+    if (password_verify($pass, $db_password)) {
+        session_start();
+        $_SESSION['userData']['first_name'] = $first_name; 
+        header("Location: ../simple_UI.php");
+        exit();
+    } else {
+        $error_msg = 'Wrong Password!';
+        header("Location: login.php?error=$error_msg");
+        exit();
+    }
+}
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+    $conn = null;
+
+
+
+?>
