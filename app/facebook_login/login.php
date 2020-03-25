dc3240cbf84918adec1b8aad5b8bca097f7a4610
Josiah <josiahwong.2018@smu.edu.sg>
2020-03-25 23:19:39 +0800

    CSS for landing pages

--- app/facebook_login/login.php
+++ app/facebook_login/login.php
@@ -14,20 +14,32 @@ $fullURL = $handler->getLoginUrl($redirectTo, $data);
 
 <!DOCTYPE html>
 <html>
-  <link rel="stylesheet" type="text/css" href="../../css/style.css">
+<body>
+  <link rel="stylesheet" type="text/css" href="../../css/welcome.css">
+    <div class="sign">
+      <span class="fast-flicker">WELCOME&nbsp</span>B<span class="flicker">A</span>CK
+    </div>
+  <link rel="stylesheet" type="text/css" href="../../css/quote.css">
+  <div class="container">
+    <div class="typewriter">
+     <h1>Let's find a Cafe!</h1>
+    </div>
+</div>
+  </body>
+  <link rel="stylesheet" type="text/css" href="../../css/landing.css">
 
         <form id="msform" method="post" action="login_logic.php">
         <fieldset>
-            <label for="exampleInputEmail1">Email address</label>
+
             <input type="email" class="form-control" name="loginEmail" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-            <br><br>
-            <label for="exampleInputPassword1">Password</label>
+
+            <br>
+
             <input type="password" class="form-control" name="loginPassword" placeholder="Password">
           
           
-          <input type="submit" value="Login" class="btn btn-primary">
-          <input type="button" onclick="window.location = '<?php echo $fullURL ?>'" value="Login with Facebook" class="btn btn-primary">
+          <input type="submit" value="Login" class="btn btn-primary action-button">
+          <input type="button" onclick="window.location = '<?php echo $fullURL ?>'" value="Login with Facebook" class="btn btn-primary action-button">
           <?php if (isset($_GET["error"])){
                 echo $_GET['error'];
                 } ?>  
@@ -37,5 +49,9 @@ $fullURL = $handler->getLoginUrl($redirectTo, $data);
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
         <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js'></script>
         <script src="login_animation.js"></script>
+
+        
+        <button id="redirection" onclick="window.location='../login.html';">Sign Me Up!</button>
+        
 </html>
 
