889f6c34a077236ce4c5d60695670fa03223bfbe
qijintay <qijintay@users.noreply.github.com>
2020-04-02 17:05:02 +0800

    Update simple_UI.php

--- app/simple_UI.php
+++ app/simple_UI.php
@@ -221,6 +221,7 @@
                                 "<p>" +"<i class='fa fa-map-marker' aria-hidden='true'></i>"+"&nbsp"+ cafe.location + "</p>" +
                                 "<a id='bookBtn' class='btn btn-primary' href='bookingcalendar/index.php?cafename=" + cafe.name +  "'>Book now!</a>"+
                                 "<a id='viewReviewBtn' class='btn btn-primary' href='viewreviews.php?cafeid=" + cafe.cafeID + "'>View Reviews</a>" +
+                                "<a id='locationBtn' class='btn btn-primary' target='_blank' href='directions.html?key="+cafe.name+"'>Get Here!</a>" +
                                 "</div>"+
                                 "</div>"; 
                               rows += eachRow ;
