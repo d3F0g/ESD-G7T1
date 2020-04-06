7c41bbf9db2472636a7e44bf75a8ce1721b6e89c
Josiah <josiahwong.2018@smu.edu.sg>
2020-04-06 19:28:19 +0800

    Update esd.sql

--- sql/esd.sql
+++ sql/esd.sql
@@ -21,6 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `esd`
 --
+DROP DATABASE IF EXISTS `esd`;
 CREATE DATABASE IF NOT EXISTS `esd` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `esd`;
 
@@ -44,6 +45,12 @@ CREATE TABLE IF NOT EXISTS `booking` (
   KEY `cafeID` (`cafeID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+Insert into booking
+(ID, userID, cafeID, seat_no, block,date, status)
+values (1,1,1,"1A",6,"2020-04-02","Confirmed"),
+(2,3,2,"6A",3,"2020-04-02","Confirmed"),
+(3,2,1,"8A",3,"2020-04-02","Confirmed");
+
 -- --------------------------------------------------------
 
 --
@@ -63,6 +70,13 @@ CREATE TABLE IF NOT EXISTS `cafes` (
   `location` varchar(50) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+Insert into cafes
+(ID, name, email, password, phone, poc, avg_review, price, location)
+values (1, "Lola","kindadopey@gmail.com", "$2y$10$muXf58lihCL6cmObNmqBH.nr/szrmmrPw3MfQ2dpL/5JZCkM0EGbu","65353535",'Lola', 3, 1, "Bukit Josiah"),
+(2, "Kooks","hxchoo@gmail.com","$2y$10$muXf58lihCL6cmObNmqBH.nr/szrmmrPw3MfQ2dpL/5JZCkM0EGbu", "90013113",'Kook', 5,5,"Bukit Lvin"),
+(3, "Josies","josie@gmail.com", "$2y$10$muXf58lihCL6cmObNmqBH.nr/szrmmrPw3MfQ2dpL/5JZCkM0EGbu","99009678",'Josie', 3, 1, "Bukit Josie"),
+(4, "Joqeewee","joqeewee@gmail.com", "$2y$10$muXf58lihCL6cmObNmqBH.nr/szrmmrPw3MfQ2dpL/5JZCkM0EGbu","81880092",'Joqeewee', 3, 1, "Bukit Joqeewee");
 
 -- --------------------------------------------------------
 
@@ -123,6 +137,12 @@ CREATE TABLE IF NOT EXISTS `reviews` (
   KEY `bookingID` (`bookingID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+Insert into reviews
+(ID, userID, cafeID, bookingID, content, stars)
+values (1,1,1,1,"The service is excellent. Food was quite good. Ordered the Spinach and Mushroom Quiche and bread was on the crispy side which was delightful. However the serving size for all the dishes we ordered were quite little and I was unfortunately not filled after the meal.",4.25),
+(2,3,2,2, "Rude server! He literally was telling me and my friend that he don't want to serve us and was not keen to take our orders. Stay away from this place. Don't spend your money to buy humiliation!", 1.05),
+(3,2,1,3, "Such a disappointment. Took them 15 mins to inform that the plain waffles are not available. After we asked for the chocolate waffles, we waited for more than 30 mins before we were informed again that they are unable to produce the waffles. Seriously?? I have been looking at the 3 staff mending at the waffle machine. I am sure something could be done better with their services!", 2.45);
+
 -- --------------------------------------------------------
 
 --
@@ -140,6 +160,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `social_media` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+Insert into users 
+(ID, email, password, first_name, last_name, phone, social_media)
+values (1, "joqeewee@gmail.com","$2y$10$muXf58lihCL6cmObNmqBH.nr/szrmmrPw3MfQ2dpL/5JZCkM0EGbu", "Josiah", "Wong", "98575987",NULL),
+(2,"lvin_tank_esd@gmail.com", "$2y$10$muXf58lihCL6cmObNmqBH.nr/szrmmrPw3MfQ2dpL/5JZCkM0EGbu", "Lvin", "Shao", "95595559", NULL),
+(3,"stay_away_qijin@gmail.com", "$2y$10$muXf58lihCL6cmObNmqBH.nr/szrmmrPw3MfQ2dpL/5JZCkM0EGbu", "Qijon", "Tay", "65353535", NULL);
 
 --
 -- Constraints for dumped tables
